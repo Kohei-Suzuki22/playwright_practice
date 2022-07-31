1a30748506078f4dbf38c528def58a4e86d96594
kohei <bhkbb0551@gmail.com>
2022-07-31 23:05:14 +0900

    projects format which easy to reuse

---
+++ test_home_page_layout.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from home_page_elements import HomePage
+from shop_women_elements import ShopWomem
+
+
+def about_us_selection_verbiage(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+
+    shop_women = ShopWomem(page)
+    home_page = HomePage(page)
+    expect(home_page.celebrate_header).to_be_visible()
+    expect(home_page.celebrate_body).to_be_visible()
+    expect(shop_women.celebrating_beauty_header).to_be_visible()
+    expect(shop_women.celebrating_beauty_body).to_be_visible()
+
+
+with sync_playwright() as playwright:
+    about_us_selection_verbiage(playwright)
