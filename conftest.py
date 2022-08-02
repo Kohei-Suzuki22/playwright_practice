66b85f74544a47888450613980875dc1209ab8a5
kohei <bhkbb0551@gmail.com>
2022-08-03 01:04:02 +0900

    set auto page instance

--- conftest.py
+++ conftest.py
@@ -4,10 +4,7 @@ import pytest
 
 
 @pytest.fixture
-def set_up(playwright: Playwright):
-    browser = playwright.chromium.launch()
-    context = browser.new_context()
-    page = context.new_page()
+def set_up(page):
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
 
-    yield  page
+    yield page
