74428c27fc80f146e17c51d9d2b63be67944b515
kohei <bhkbb0551@gmail.com>
2022-08-07 20:06:39 +0900

    add xfail-case by pytest.mark.parametrize

--- tests_ui_layout/test_contact_us_from.py
+++ tests_ui_layout/test_contact_us_from.py
@@ -6,13 +6,17 @@ from pom.contact_us_page import ContactUsPage
 
 
 ## parameterizeはテストメソッドにつけることができる。 fixtureにはつけたら読み込まれなかった。
-@pytest.mark.parametrize("email, num", [("foo@gmail.com",1),("bar@gmail.com",2),("foo@gmail.com",3)])
-def test_submit_form(set_up,email,num) -> None:
+@pytest.mark.parametrize("email, num", [
+                          # パラメータによっては、xfailにしたいものが出てくる。その場合は、以下のように設定。
+                          pytest.param("foo@gmail.com", 1, marks=pytest.mark.xfail(reason='wrong calc')),
+                          ("bar@gmail.com", 2),
+                          pytest.param("foo@gmail.com", 3, marks=pytest.mark.xfail(reason='wrong calc'))
+                          ])
+def test_submit_form(set_up, email, num) -> None:
 
     page = set_up
     contact_us = ContactUsPage(page)
     contact_us.navigate()
 
     contact_us.submit_form('taro', 'my address', email, '111-1111-1111', 'test subject', 'test message blah')
-    time.sleep(2)
     assert (1+1) == num
