c701412917c06e0d47d5d3edcd48dbd91d4c04c0
kohei <bhkbb0551@gmail.com>
2022-08-29 02:18:35 +0900

    add assert_snapshot option  'full_page=True'

--- visual_tests/test_landing_page_visual.py
+++ visual_tests/test_landing_page_visual.py
@@ -7,7 +7,7 @@ def test_visual_landing1_1(page, assert_snapshot) -> None:
     page.goto("https://symonstorozhenko.wixsite.com/website-1/shop")
     homepage = HomePage(page)
     # expect(homepage.celebrate_header).to_be_visible()
-    assert_snapshot(page.screenshot())
+    assert_snapshot(page.screenshot(full_page=True))
 
 
 def test_visual_landing1_2(page, assert_snapshot) -> None:
