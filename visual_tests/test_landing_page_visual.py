71675be19ad53c9d8a2fc24d0819dc07945430d2
kohei <bhkbb0551@gmail.com>
2022-08-29 01:32:23 +0900

    add pytest-playwright-visual  assert_snapshot

---
+++ visual_tests/test_landing_page_visual.py
@@ -0,0 +1,17 @@
+from pom.home_page_elements import HomePage
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+
+def test_visual_landing1_1(page, assert_snapshot) -> None:
+    page.goto("https://symonstorozhenko.wixsite.com/website-1/shop")
+    homepage = HomePage(page)
+    # expect(homepage.celebrate_header).to_be_visible()
+    assert_snapshot(page.screenshot())
+
+
+def test_visual_landing1_2(page, assert_snapshot) -> None:
+    page.goto("https://symonstorozhenko.wixsite.com/website-1/shop")
+    homepage = HomePage(page)
+    # expect(homepage.celebrate_header).to_be_visible()
+    assert_snapshot(page.screenshot())
